c07c1aeb3f355e948e3a734b0a04abeeeb4575f9
madukapcm <frankkessi2020@gmail.com>
2022-08-01 02:37:09 +0300

    maduka schema on maduka model query

--- frank/maduka/schema.py
+++ frank/maduka/schema.py
@@ -7,6 +7,10 @@ class FrankType(DjangoObjectType):
         model = Frank
         fields = ('id','name')
 
+class MadukaType(DjangoObjectType):
+    class Meta:
+        model = Maduka
+
 #the data in the above class are translated into graph like
 # type Frank{
 #     id: id
@@ -16,8 +20,12 @@ class FrankType(DjangoObjectType):
 class Query(graphene.ObjectType):
 
     all_frank = graphene.List(FrankType)
+    all_maduka = graphene.List(MadukaType)
 
     def resolve_all_frank(root, info):
         return Frank.objects.all()
+
+    def resolve_all_maduka(root, info):
+        return Maduka.objects.all()
 
 schema = graphene.Schema(query=Query)
