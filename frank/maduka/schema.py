77a9c9c98663e1707b12b2b3715e4d9f3524de24
madukapcm <frankkessi2020@gmail.com>
2022-08-01 12:08:30 +0300

    mutation

--- frank/maduka/schema.py
+++ frank/maduka/schema.py
@@ -5,7 +5,7 @@ from . models import Frank,Maduka
 class FrankType(DjangoObjectType):
     class Meta:
         model = Frank
-        fields = ('id','name')
+        # fields = ('id','name')
 
 class MadukaType(DjangoObjectType):
     class Meta:
@@ -17,6 +17,42 @@ class MadukaType(DjangoObjectType):
 #     name: String
 # }
 
+#a mutation class for creating new instance of frank
+class CreateFrank(graphene.Mutation):
+    frank = graphene.Field(FrankType)
+
+    class Arguments:
+        name = graphene.String()
+
+    def mutate(self, info, name):
+        datac = Frank(name=name)
+        datac.save()
+        return CreateFrank(frank=datac)
+
+#a slass instance for creating new instance of maduka.
+class CreateMaduka(graphene.Mutation):
+    maduka = graphene.Field(MadukaType)
+
+    class Arguments:
+        name = graphene.String()
+        notes = graphene.String()
+        frank_id = graphene.Int()
+
+    def mutate(self, info, name,notes,frank_id):
+        datac = Maduka(name=name,notes=notes)
+        data = Frank.objects.get(id=frank_id)
+        datac.frank = data
+        datac.save()
+
+        return CreateMaduka(maduka=datac)
+
+#main mutation class.... for object instance creations.
+class Mutation(graphene.ObjectType):
+    create_maduka = CreateMaduka.Field()
+    create_frank = CreateFrank.Field()
+
+
+#a class for data querying from our related models...
 class Query(graphene.ObjectType):
 
     all_frank = graphene.List(FrankType)
@@ -28,4 +64,4 @@ class Query(graphene.ObjectType):
     def resolve_all_maduka(root, info):
         return Maduka.objects.all()
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
